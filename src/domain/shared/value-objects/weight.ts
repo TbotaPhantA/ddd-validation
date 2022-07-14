cc940008761f30d710064e913c340329d2effa5d
TbotaPhantA <botakiller@mail.ru>
2022-07-14 10:41:02 +0000

    weight value object was refactored for canCreate interface

--- src/domain/shared/value-objects/weight.ts
+++ src/domain/shared/value-objects/weight.ts
@@ -3,7 +3,8 @@ import {
   UNKNOWN_WEIGHT_UNIT,
   WEIGHT_CANNOT_BE_NEGATIVE,
 } from '../errors/error-messages';
-import { assert } from '../utils/assert';
+import * as E from 'fp-ts/Either';
+import { isLeft } from 'fp-ts/Either';
 
 export type WeightUnit = 'kg';
 
@@ -11,13 +12,38 @@ export class Weight {
   private readonly weightInKilograms: number;
 
   constructor(weight: number, unit: WeightUnit) {
-    assert(weight >= 0, WEIGHT_CANNOT_BE_NEGATIVE);
+    const canCreate = Weight.canCreate(weight, unit);
+
+    if (isLeft(canCreate)) {
+      throw new DomainError(canCreate.left.join('; '));
+    }
 
     if (unit === 'kg') {
       this.weightInKilograms = weight;
     } else {
       throw new DomainError(UNKNOWN_WEIGHT_UNIT(unit));
     }
+  }
+
+  public static canCreate(
+    weight: number,
+    unit: WeightUnit,
+  ): E.Either<string[], undefined> {
+    const errors: string[] = [];
+
+    if (weight < 0) {
+      errors.push(WEIGHT_CANNOT_BE_NEGATIVE);
+    }
+
+    if (unit !== 'kg') {
+      errors.push(UNKNOWN_WEIGHT_UNIT(unit));
+    }
+
+    if (errors.length > 0) {
+      return E.left(errors);
+    }
+
+    return E.right(undefined);
   }
 
   get kilograms() {
