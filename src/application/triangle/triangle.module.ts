57d15171480c27a644f7fe8f3b0633938ac09197
TbotaPhantA <botakiller@mail.ru>
2022-07-23 22:18:58 +0000

    Triangle entity was added

--- src/application/triangle/triangle.module.ts
+++ src/application/triangle/triangle.module.ts
@@ -4,9 +4,10 @@ import { TriangleController } from './triangle.controller';
 import { TriangleService } from './triangle.service';
 import { TriangleFactory } from './triangle.factory';
 import { TriangleRepository } from './triangle.repository';
+import { Triangle } from '../../domain/triangle/triangle';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([])],
+  imports: [TypeOrmModule.forFeature([Triangle])],
   controllers: [TriangleController],
   providers: [TriangleService, TriangleFactory, TriangleRepository],
 })
