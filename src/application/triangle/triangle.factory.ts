a09c16f32dde130733d1454e97e2066d24c28565
TbotaPhantA <botakiller@mail.ru>
2022-07-23 22:26:13 +0000

    canCreate check was added to factory

--- src/application/triangle/triangle.factory.ts
+++ src/application/triangle/triangle.factory.ts
@@ -1,19 +1,21 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { TriangleRepository } from './triangle.repository';
 import { CreateTriangleInputDto } from './dto/create-triangle-dto/create-triangle-input.dto';
 import { Triangle } from '../../domain/triangle/triangle';
+import { isFail } from '@derbent-ninjas/invariant-composer';
 
 @Injectable()
 export class TriangleFactory {
   constructor(private readonly triangleRepository: TriangleRepository) {}
 
   public create(dto: CreateTriangleInputDto): Triangle {
+    // TODO: make real queires to DB
     const isNameUnique = false;
     const isSideAUnique = false;
     const isSideBUnique = false;
     const isSideCUnique = false;
 
-    return new Triangle(dto, {
+    const extraValidationParams = {
       nameData: {
         isUnique: isNameUnique,
       },
@@ -28,6 +30,16 @@ export class TriangleFactory {
           isUnique: isSideCUnique,
         },
       },
-    });
+    };
+
+    const canCreate = Triangle.canCreate(dto, extraValidationParams);
+
+    if (isFail(canCreate)) {
+      // TODO: create assert function which throws special error,
+      //  then exception filter receives this error and properly displays at response
+      throw new BadRequestException('cannot create');
+    }
+
+    return new Triangle(dto, extraValidationParams);
   }
 }
